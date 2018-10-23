99cd34c2b895c36376e4f57dafdff2f48c511532
uziassantosferreira <uziasferreira@gmail.com>
2018-10-22 22:21:57 -0300

    add animation in fragments

--- app/src/main/kotlin/com/doghero/features/menu/MenuActivity.kt
+++ app/src/main/kotlin/com/doghero/features/menu/MenuActivity.kt
@@ -1,6 +1,7 @@
 package com.doghero.features.menu
 
 import android.os.Bundle
+import androidx.navigation.NavOptions
 import com.doghero.R
 import com.doghero.base.BaseActivity
 import androidx.navigation.Navigation
@@ -18,6 +19,10 @@ class MenuActivity : BaseActivity() {
     }
 
     private fun setupNavigation() {
+        val options = NavOptions.Builder()
+            .setEnterAnim(R.anim.nav_default_enter_anim)
+            .setExitAnim(R.anim.nav_default_exit_anim)
+            .build()
         val navController = Navigation.findNavController(this, R.id.mainNavigationFragment)
         setupActionBarWithNavController(this, navController)
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
