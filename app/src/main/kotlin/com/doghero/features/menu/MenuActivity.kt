d57e4c5d477b057ecd16742d8edd6c28e049006e
uziassantosferreira <uziasferreira@gmail.com>
2018-10-22 22:22:07 -0300

    add toolbar and theme to him

--- app/src/main/kotlin/com/doghero/features/menu/MenuActivity.kt
+++ app/src/main/kotlin/com/doghero/features/menu/MenuActivity.kt
@@ -1,7 +1,6 @@
 package com.doghero.features.menu
 
 import android.os.Bundle
-import androidx.navigation.NavOptions
 import com.doghero.R
 import com.doghero.base.BaseActivity
 import androidx.navigation.Navigation
@@ -14,15 +13,12 @@ class MenuActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         setupNavigation()
     }
 
     private fun setupNavigation() {
-        val options = NavOptions.Builder()
-            .setEnterAnim(R.anim.nav_default_enter_anim)
-            .setExitAnim(R.anim.nav_default_exit_anim)
-            .build()
         val navController = Navigation.findNavController(this, R.id.mainNavigationFragment)
         setupActionBarWithNavController(this, navController)
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
